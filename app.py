9814ee32f8564a5925e472caa411b45895391ead
LuisG-Custodio <121039302@upq.edu.mx>
2023-07-07 07:48:43 -0600

    Creacion de rutas de agregado a base de datos
    Se crearon las rutas para poder agregar informacion en la base de datos,

--- app.py
+++ app.py
@@ -10,11 +10,21 @@ app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='Vunity'
 app.secret_key='mysecretkey'
-MySQL=MySQL(app)
-#declaracion o inicializacion de las rutas y le pertenece a http://localhost:5000
+mysql=MySQL(app)
+
+
 @app.route('/')
 def inicio():
     return render_template('inicio.html')
+
+@app.route('/login', methods=['POST'])
+def guardar():
+    if request.method == 'POST':
+        CC= mysql.connection.cursor()
+        CC.execute()
+        mysql.connection.commit()           
+    flash()
+    return redirect(url_for('index'))
 
 @app.route('/index')
 def index():
@@ -24,13 +34,52 @@ def index():
 def autos():
     return render_template('Autos.html')
 
+@app.route('/guardarautos', methods=['POST'])
+def guardarautos():
+    if request.method == 'POST':
+        Vmatricula=request.form['txtMatricula']
+        Vmarca=request.form['txtMarca']
+        Vpoliza=request.form['txtPoliza']
+        Vcapacidad=request.form['txtCapacidad']
+        Vtipo=request.form['txtTipo']
+        CS= mysql.connection.cursor()
+        CS.execute('insert into tb_autos(matricula,marca,poliza,capacidad,tipo) values(%s,%s,%s,%s,%s)',(Vmatricula,Vmarca,Vpoliza,Vcapacidad,Vtipo))
+        mysql.connection.commit()           
+    flash('Los datos del auto fueron agregados correctamente')
+    return redirect(url_for('index'))
+
 @app.route('/personas')
 def personas():
     return render_template('personas.html')
 
+@app.route('/guardarpersonas', methods=['POST'])
+def guardarpersonas():
+    if request.method == 'POST':
+        Vnombre=request.form['txtNombre']
+        Vap=request.form['txtAP']
+        Vam=request.form['txtAM']
+        Vtelefono=request.form['txtTelefono']
+        Vfecha=request.form['txtFecha']
+        CS= mysql.connection.cursor()
+        CS.execute('insert into tb_personas(nombre,ap,am,telefono,fecha_nac) values(%s,%s,%s,%s,%s)',(Vnombre,Vap,Vam,Vtelefono,Vfecha))
+        mysql.connection.commit()           
+    flash('Los datos de la persona fueron agregados correctamente')
+    return redirect(url_for('index'))
+
 @app.route('/viajes')
 def viajes():
     return render_template('Viajes.html')
+
+@app.route('/crearviaje', methods=['POST'])
+def crearviaje():
+    if request.method == 'POST':
+        Vruta=request.form['txtRuta']
+        Vparada=request.form['txtParada']
+        CS= mysql.connection.cursor()
+        CS.execute('insert into tb_viajes(ruta,parada) values(%s,%s)',(Vruta,Vparada))
+        mysql.connection.commit()           
+    flash('El viaje fue creado correctamente')
+    return redirect(url_for('index'))
     
 
 #ejecucion del servidor y asignacion del puerto a trabajar
