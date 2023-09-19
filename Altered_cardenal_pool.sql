4eb6d8d3359efafeecddcd5ee3382ec67c587091
LuisG-Custodio <121039302@upq.edu.mx>
2023-09-19 10:07:56 -0600

    Create Altered_cardenal_pool.sql

---
+++ Altered_cardenal_pool.sql
@@ -0,0 +1,92 @@
+use cpool_tryout;
+
+
+create table Conductor(
+id_conductor int not null primary key auto_increment,
+matricula varchar(9),
+primer_ingreso_flag bit default 0,
+telefono varchar(10),
+##foto binary,
+##INE binary,
+##CredencialUPQ binary,
+foreign key (matricula) references Placeholder(matricula)
+);
+
+create table Autos(
+id_auto int not null primary key auto_increment,
+placa varchar(200),
+modelo varchar(200),
+marca varchar(200),
+color varchar(200),
+poliza varchar(200),
+fecha_vencimiento date,
+lugares_disponibles int,
+##Tarjeta_circulacion binary,
+capacidad int,
+espacios_disponibles int,
+estatus boolean
+);
+
+create table Relacion_autos(
+id int not null primary key auto_increment,
+id_conductor int,
+id_auto int,
+foreign key (id_conductor) references Conductor(id_conductor) on delete cascade on update cascade,
+foreign key (id_auto) references Autos(id_auto) on delete cascade on update cascade
+);
+
+create table Ruta(
+id_ruta int not null primary key auto_increment,
+destino text not null
+);
+
+create table Referencias(
+id_referencias int not null primary key auto_increment,
+paradas text not null,
+id_ruta int,
+foreign key (id_ruta) references Ruta(id_ruta) on delete cascade on update cascade
+);
+
+create table Relacion_ruta(
+id int not null primary key auto_increment,
+id_ruta int,
+id_referencias int,
+foreign key (id_ruta) references Ruta(id_ruta) on delete cascade on update cascade,
+foreign key (id_referencias) references Referencias(id_referencias) on delete cascade on update cascade
+);
+
+create table Horarios(
+id_horario int,
+id_conductor int,
+dia varchar(10),
+turno varchar(10),
+salida time,
+id_relacion int,
+foreign key (id_relacion) references Relacion_ruta(id) on delete cascade on update cascade
+);
+
+create table Conductor(
+id_conductor int not null primary key auto_increment,
+matricula varchar(9),
+primer_ingreso_flag bit default 0,
+telefono varchar(10),
+##foto binary,
+##INE binary,
+##CredencialUPQ binary,
+foreign key (matricula) references Placeholder(matricula)
+);
+
+create table Pasajero(
+id_pasajero int not null primary key auto_increment,
+matricula varchar(9),
+telefono varchar(10),
+id_conductor int,
+aprovacion_flag int default 0,
+foreign key (matricula) references Placeholder(matricula),
+foreign key (id_conductor) references Conductor(id_conductor) on delete cascade on update cascade
+);
+
+create table Administrador(
+id varchar(15),
+password binary
+);
